15e12ba8a370b1dcdd7b159caf4c12a967ea4572
Vitor Martins <vitor.martins.dev@gmail.com>
2025-04-07 14:25:30 -0300

    Pratica de DOM

---
+++ Exercicios/AlterarElemento/index.js
@@ -0,0 +1,17 @@
+const btnAlterar = document.getElementById("btnAlterar")
+
+btnAlterar.addEventListener('click', clicar)
+
+function clicar(){
+    window.alert("teste")
+    var titulo = document.getElementById("textoAlterar")
+    var inputAlterar = document.getElementById("alterar").value
+
+    var titulo = inputAlterar
+}
+
+    
+
+
+
+    
